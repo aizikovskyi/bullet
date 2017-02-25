36f874aaf3ed75bbd854fd144fd4308d3a84f31a
Nick Aizikovskyi <aizikovskyi@users.noreply.github.com>
2017-02-24 22:34:44 -0800

    Removing inertia, because it feels awful

--- spriteobject.js
+++ spriteobject.js
@@ -99,41 +99,49 @@ class PlayerObject extends MovingObject {
     const initialVelocity = new Point(0, 0);
     super(x, y, initialVelocity, birthFrame);
     this.radius = 1;
-    this.maxSpeed = 2;
-    this.accel = 0.3;
-    this.maxAccel = 0.5;
+    // ACCEL:
+    // this.maxSpeed = 2;
+    // this.accel = 0.3;
+    // this.maxAccel = 0.5;
+    // NO ACCEL:
+    this.maxSpeed = 1.0;
   }
 
   moveTowards(targetPoint) {
     this.x += this.velocity.x;
     this.y += this.velocity.y;
     if (targetPoint) {
-      // If we are close enough to target point, just move directly onto it.
-      // This ensures we don't oscillate around it, which looks stupid
       if (this.distanceToPoint(targetPoint) < this.maxSpeed) {
+        // If we are close enough to target point, just move directly onto it.
+        // This ensures we don't oscillate around it, which looks stupid
         this.velocity = this.vectorTowards(targetPoint);
       }
       else {
-        // const vector = this.unitVectorTowards(targetPoint);
-        const curSpeed = this.velocity.distanceFromOrigin();
-        const accel = 0.2 + (curSpeed / 6);
+        // ACCEL:
+        // const curSpeed = this.velocity.distanceFromOrigin();
+        // const accel = 0.2 + (curSpeed / 6);
+        //
+        // const newLoc = new Point(this.x + this.velocity.x, this.y + this.velocity.y);
+        // const vector = newLoc.unitVectorTowards(targetPoint);
+        // const newVel = new Point(this.velocity.x + vector.x * accel, this.velocity.y + vector.y * accel);
+        // const newSpeed = newVel.distanceFromOrigin();
+        // if (newSpeed > this.maxSpeed) {
+        //   newVel.scaleBy(this.maxSpeed / newSpeed);
+        // }
+        // this.velocity = newVel;
 
-        const newLoc = new Point(this.x + this.velocity.x, this.y + this.velocity.y);
-        const vector = newLoc.unitVectorTowards(targetPoint);
-        const newVel = new Point(this.velocity.x + vector.x * accel, this.velocity.y + vector.y * accel);
-        const newSpeed = newVel.distanceFromOrigin();
-        if (newSpeed > this.maxSpeed) {
-          // Simple solution: newVel.scaleBy(this.maxSpeed / newSpeed);
-          // Instead:
-          newVel.scaleBy(this.maxSpeed / newSpeed);
-        }
-        this.velocity = newVel;
+        // NO ACCEL:
+        this.velocity = this.unitVectorTowards(targetPoint);
+        this.velocity.scaleBy(this.maxSpeed);
       }
     }
     else {
-      this.velocity.scaleBy(Math.max(0, (this.velocity.distanceFromOrigin() - this.maxAccel) / this.velocity.distanceFromOrigin()));
-      // this.velocity.x = 0;
-      // this.velocity.y = 0;
+      // ACCEL:
+      // this.velocity.scaleBy(Math.max(0, (this.velocity.distanceFromOrigin() - this.maxAccel) / this.velocity.distanceFromOrigin()));
+
+      // NO ACCEL:
+      this.velocity.x = 0;
+      this.velocity.y = 0;
     }
   }
 }
